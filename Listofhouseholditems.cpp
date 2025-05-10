e3de5f5af96194c81ba4286786cc5fb293d80946
FaisalAwan-git <faisalmlik9211@gmail.com>
2025-05-10 17:43:46 +0500

    Create Listofhouseholditems.cpp

---
+++ Listofhouseholditems.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<string> items = {
+        "Sofa",
+        "Television",
+        "Microwave",
+        "Table",
+        "Chair",
+        "Lamp",
+        "Fan",
+        "Carpet",
+        "Curtains"
+    };
+
+    for (const string& item : items) {
+        cout << item << endl;
+    }
+
+    return 0;
+}
